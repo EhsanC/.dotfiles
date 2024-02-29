633bb2d952dffc98d3c0111bc70e7a9282a396c8
Ehsan <ar.charoliaehsan@gmail.com>
2024-03-01 00:03:42 +0530

    test complete

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
         set fish_greeting
 
-# Test Line
 #-----------------Paths--------------------
 
 set PATH /home/ehsan/.local/bin $PATH
